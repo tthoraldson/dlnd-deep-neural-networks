13f904351e7edffbb160116397a8cdcac88863de
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-10-04 11:22:30 -0500

    basic stuffs

---
+++ main.py
@@ -0,0 +1,16 @@
+# Imports the MNIST dataset
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets(".", one_hot=True, reshape=False)
+
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 128  # Decrease batch size if you don't have enough memory
+display_step = 1
+
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+print("it workin")
